73ce52241db4fc96da0a9af6745ab33ed0278159
Dennis Johnson <songawee@gmail.com>
2017-08-16 13:56:02 -0600

    make smallah

--- demo/index.js
+++ demo/index.js
@@ -1,18 +1,1 @@
-const moment = require("moment");
-
-console.log(moment().toISOString());
-
-console.log("a");
-console.log("b");
-console.log("c");
 console.log("d");
-
-function test() {
-  console.log("yo!");
-}
-function hai() {
-  console.log("hai!");
-}
-
-test();
-hai();
