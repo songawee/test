9a39226d51f8f57a61278f3d19576217d5427083
Dennis Johnson <songawee@gmail.com>
2017-08-18 13:20:31 -0600

    reset master branch

--- app/index.js
+++ app/index.js
@@ -1,6 +1,1 @@
-require("./styles/styles.css");
-
-const rxjs = require("rxjs");
-
-console.log(rxjs.AsyncSubject);
 console.log("test");
--- app/other.js
+++ app/other.js
@@ -1,7 +1,1 @@
-const moment = require("moment");
-
 console.log("hai there");
-
-import(/* webpackChunkName: "dynamic" */ "./dynamic").then(module => {
-  console.log(module);
-});
