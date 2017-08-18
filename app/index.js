2aaf5c5b6f1ea5b24d390fe92b4e7e64feb23b5e
Dennis Johnson <songawee@gmail.com>
2017-08-18 13:59:11 -0600

    add more deps

--- app/index.js
+++ app/index.js
@@ -1,5 +1,8 @@
 require("./styles/styles.css");
+require("./styles/more.css");
 
+const _ = require("lodash");
 const rxjs = require("rxjs");
 
 console.log(rxjs.Observable);
+console.log(_.cloneDeep("foo"));
